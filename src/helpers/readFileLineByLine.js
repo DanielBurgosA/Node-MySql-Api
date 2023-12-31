252c6cdec168e66d885974c31cab6e6a6d65d38e
Daniel Burgos <dburgos.developer@gmail.com>
2023-12-31 18:21:20 -0500

    feat: Installed JSONStream dependency and optimized read line logic

--- src/helpers/readFileLineByLine.js
+++ src/helpers/readFileLineByLine.js
@@ -1,27 +1,58 @@
 const fs = require('fs');
-const readline = require('readline');
+const JSONStream = require('JSONStream');
 
-async function processFile() {
-    const readStream = fs.createReadStream('../../large_products.jsonl');
-    const rl = readline.createInterface({
-        input: readStream,
-        crlfDelay: Infinity,
-    });
+const processJSONLFile = ()=>{
+    const readStream = fs.createReadStream('../../large_products.jsonl', { encoding: 'utf8' });
 
-    let lineC = 0;
+    let lineCount = 0;
 
-    for await (const line of rl) {
-        console.log(`line ${lineC} ${line}`);
-        lineC++;
-        if (lineC >= 2) {
-            rl.close();
-            break;
-        }
-        // const data = JSON.parse(line);
-    }
-
-    console.log('File read complete');
-
+    readStream
+        .pipe(JSONStream.parse())
+        .on('data', (data) => {
+            if (lineCount < 10000) {
+                console.log(`Line ${lineCount}:`, data);
+                lineCount++;
+            } else {
+                readStream.unpipe(JSONStream);
+                readStream.destroy();
+            }
+        })
+        .on('end', () => {
+            console.log('File read complete');
+        })
+        .on('error', (err) => {
+            console.error('Error reading file:', err);
+        });
 }
 
-processFile();
+processJSONLFile()
+
+module.exports = processJSONLFile;
+
+// const fs = require('fs');
+// const readline = require('readline');
+
+// async function processFile() {
+//     const readStream = fs.createReadStream('../../large_products.jsonl');
+//     const rl = readline.createInterface({
+//         input: readStream,
+//         crlfDelay: Infinity,
+//     });
+
+//     let lineC = 0;
+
+//     for await (const line of rl) {
+//         console.log(`line ${lineC} ${line}`);
+//         lineC++;
+//         if (lineC >= 2) {
+//             rl.close();
+//             break;
+//         }
+//         // const data = JSON.parse(line);
+//     }
+
+//     console.log('File read complete');
+
+// }
+
+// processFile();
